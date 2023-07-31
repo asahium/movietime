34e1521838e26fe2005b15df9236f568346a152c
baked bean <doodwyrm@protonmail.com>
2023-07-31 20:47:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = '1673808547:AAEBXVWluLXNQ7YSXiVYHuffal2ST3QrkNk'
+TOKEN = ''
 # bot token from @BotFather
-KP_TOKEN = '44bad1a2-ffd7-4bbe-9ae4-add698800b2e'
+KP_TOKEN = ''
 # api token from https://kinopoiskapiunofficial.tech/signup
